1fb31f0e4d3f2550b14b391e052ac003b26482da
harismuslic04 <harismuslic16@gmail.com>
2023-01-28 16:39:30 +0100

    a

---
+++ IT camp/JAVA/40.cas/tempCodeRunnerFile.js
@@ -0,0 +1,26 @@
+const subStr = (str) => {
+//   const subStrArr = [];
+//   for (let i = 0; i < str.length; i++) {
+//     for (let k = i; k < str.length; k++) {
+//       subStrArr.push(str.slice(i, k + 1));
+//     }
+//   }
+//   return subStrArr;
+// };
+// console.log(subStr("dog"));
+
+// const student = {
+//   ime: "Bakir",
+//   prezime: "Ujkanovic",
+//   godine: 18,
+//   brojIndeksa: 036002,
+//   ocene: [8, 7, 10, 9, 9],
+//   prosekOcena: function () {
+//     const duzina = this.ocene.length;
+//     const prosekk = this.ocene.reduce(function (a, b) {
+//       return a + b;
+//     });
+//     return prosekk / duzina;
+//   },
+// };
+// console.log(student.prosekOcena());
