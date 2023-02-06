9deabe6b9807a79b1e78f3f77ae17cded200f3d7
harismuslic04 <harismuslic16@gmail.com>
2023-02-06 14:07:29 +0100

    push

--- IT camp/JAVA/44.cas/app.js
+++ IT camp/JAVA/44.cas/app.js
@@ -1,22 +1,21 @@
 const niz = [4, 5, 5, 6, 4, 9, -1, 5];
-//klonirati niz
-const niz1 = [];
-for (let i = 0; i < niz.length; i++) {
-  niz1.push(niz[i]);
-}
-console.log(niz1);
 
-// soread operator (...)
-// Pomocu njega kloniramo niz ili objekat
+// Klonirati niz.
+const niz2 = niz.map((element) => element);
+console.log(niz2);
 
-const niz3 = [...niz];
+// spread operator (...)
+// Pomocu njega kloniramo niz ili objekat.
+
+const niz3 = [120, 110, 100, ...niz];
 console.log(niz3);
 
 const obj = {
   firstName: "Haris",
   lastName: "Muslic",
-  status: "samo liveprul",
+  status: "samo liverpul",
+  age: 19,
 };
 
-const obj2 = { ...obj, status: false, age: 19 };
+const obj2 = { ...obj, status: false, age: obj.age + 1 };
 console.log(obj2);
