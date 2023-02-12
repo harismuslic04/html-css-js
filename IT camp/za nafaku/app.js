2b60aaa0ba4c09cb17594334904489438a00e89a
harismuslic04 <harismuslic16@gmail.com>
2023-02-12 18:56:44 +0100

    h

---
+++ IT camp/za nafaku/app.js
@@ -0,0 +1,8 @@
+Bismillahi rrahmani rrahim
+
+El-Hamdu lillahi rrabil-alemin
+Er-rahmanir-rahim
+Maliki jevmid-din
+ijjake n'abudu ve ijjake neste'in
+ihdines-siratel mustekim
+siratallezine en'amte alejhim gajril-magdubi'alejhim veled-dallin!
