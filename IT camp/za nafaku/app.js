b446351846e515d8a6785af26c7adb660831f0f8
harismuslic04 <harismuslic16@gmail.com>
2023-02-19 23:20:19 +0100

    nafaka

--- IT camp/za nafaku/app.js
+++ IT camp/za nafaku/app.js
@@ -1,8 +1,23 @@
 Bismillahi rrahmani rrahim
 
 El-Hamdu lillahi rrabil-alemin
+
 Er-rahmanir-rahim
+
 Maliki jevmid-din
+
 ijjake n'abudu ve ijjake neste'in
+
 ihdines-siratel mustekim
-siratallezine en'amte alejhim gajril-magdubi'alejhim veled-dallin!
+
+siratallezine en'amte alejhim gajril-magdubi'alejhim veled-dallin! 
+
+
+
+Allahu laaa ilaaha illaa huwal haiyul qai-yoom; laa taakhuzuhoo sinatunw wa laa nawm; lahoo maa fissamaawaati wa maa 
+
+fil ard; man zallazee yashfa’u indahooo illaa be iznih; ya’lamu maa baina aideehim wa maa khalfahum; wa laa yuheetoona 
+
+beshai ‘immin ‘ilmihee illa be maa shaaaa; wasi’a kursiyyuhus samaa waati wal arda wa la ya’ooduho hifzuhumaa; wa huwal 
+
+aliyyul ‘azeem
